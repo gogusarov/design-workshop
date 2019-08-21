da39cdc773b3b1c0087ed7c0486b817ddd99ba19
Grigory Gusarov <g.gusarov@tinkoff.ru>
2019-08-21 13:30:52 +0300

    Updates

---
+++ precache-manifest.dead12fc8177f5fe1eb00ce1b91a37b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e33916b4f4283a14b0faa5a9c6fcd061",
+    "url": "/design-workshop/index.html"
+  },
+  {
+    "revision": "e9c8fd5bc299c05c68fa",
+    "url": "/design-workshop/static/css/main.bc998225.chunk.css"
+  },
+  {
+    "revision": "0db2c3f1fd6dd3727ea9",
+    "url": "/design-workshop/static/js/2.060ec596.chunk.js"
+  },
+  {
+    "revision": "e9c8fd5bc299c05c68fa",
+    "url": "/design-workshop/static/js/main.64f04582.chunk.js"
+  },
+  {
+    "revision": "6a6db43ec2ca5bc768ab",
+    "url": "/design-workshop/static/js/runtime~main.d7211274.js"
+  },
+  {
+    "revision": "2999d85583ec178bc93b37e0ed2310a8",
+    "url": "/design-workshop/static/media/logo.2999d855.svg"
+  }
+]);
